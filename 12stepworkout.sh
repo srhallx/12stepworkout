8129c4d4bce04005c8f743bf76c86e51ac142875
Steve Hall <steve.hall@xamarin.com>
2015-09-20 18:18:58 -0500

    Commit

---
+++ 12stepworkout.sh
@@ -0,0 +1,22 @@
+echo "12 Step workout"
+for i in jumping_jacks wall_sit push_ups abdominal_crunch step_up_onto_chair squats triceps_dip_on_chair plank high_knees_running_in_place lunge push_up_and_rotation side_plank
+do
+	say $i
+	echo $i
+	say "starting in 10 seconds"
+	sleep 8
+	
+	say $i
+	say "start in 3"
+	say "2"
+	say "1"
+	sleep 10
+	say 10
+	sleep 10
+	say 20
+	sleep 10
+	say "done.  Rest for 10"
+	say "next exercise is "
+done
+say "you are all done."
+
